5a778b82ea34f134fb58f98d1f60b4e07b5b08b5
ankitnag0 <heroankit0@gmail.com>
2023-03-08 22:47:54 +0530

    Added book resource

---
+++ src/book/book.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class BookService {
+  constructor(private prisma: PrismaService) {}
+  async create(createBookDto: CreateBookDto) {
+    const book = await this.prisma.book.create({
+      data: {
+        title: createBookDto.title,
+        author: createBookDto.author,
+        description: createBookDto.description,
+      },
+    });
+    return book;
+  }
+
+  async findAll() {
+    const books = await this.prisma.book.findMany();
+    return books;
+  }
+
+  async findOne(id: number) {
+    const book = await this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+    return book;
+  }
+
+  async update(id: number, updateBookDto: UpdateBookDto) {
+    const book = await this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+
+    const updatedBook = await this.prisma.book.update({
+      where: {
+        id,
+      },
+      data: {
+        ...updateBookDto,
+      },
+    });
+
+    return updatedBook;
+  }
+
+  async remove(id: number) {
+    const book = await this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+
+    const deletedBook = await this.prisma.book.delete({
+      where: {
+        id,
+      },
+    });
+
+    return deletedBook;
+  }
+}
